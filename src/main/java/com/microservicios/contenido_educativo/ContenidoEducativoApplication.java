8746d2a1636276896394169a86c2ebb4c5085248
francarrascoo <fr.carrascol@duocuc.cl>
2025-05-27 00:35:31 -0400

    klk

---
+++ src/main/java/com/microservicios/contenido_educativo/ContenidoEducativoApplication.java
@@ -0,0 +1,13 @@
+package com.microservicios.contenido_educativo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContenidoEducativoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContenidoEducativoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/microservicios/contenido_educativo/ContenidoEducativoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservicios.contenido_educativo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContenidoEducativoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
