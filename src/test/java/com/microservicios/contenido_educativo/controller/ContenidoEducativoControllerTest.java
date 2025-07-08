1719433c89e913813b40f02140e4cb733457da5d
francarrascoo <fr.carrascol@duocuc.cl>
2025-07-07 20:15:11 -0400

    controller tests

--- src/test/java/com/microservicios/contenido_educativo/controller/ContenidoEducativoControllerTest.java
+++ src/test/java/com/microservicios/contenido_educativo/controller/ContenidoEducativoControllerTest.java
@@ -1,27 +1,31 @@
 package com.microservicios.contenido_educativo.controller;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doThrow;
-import static org.mockito.Mockito.when;
-
 import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Optional;
 
-import org.springframework.http.MediaType;
 import org.junit.jupiter.api.Test;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.when;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.microservicios.contenido_educativo.model.ContenidoEducativo;
 import com.microservicios.contenido_educativo.model.TipoContenido;
 import com.microservicios.contenido_educativo.service.ContenidoEducativoService;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(ContenidoEducativoController.class)
 public class ContenidoEducativoControllerTest {
@@ -128,7 +132,7 @@ public class ContenidoEducativoControllerTest {
     @Test
     void testCrearContenido_EntityNotFoundException() throws Exception {
         ContenidoEducativo input = contenidoMock();
-        input.setContId(null); // para crear contenido nuevo el id debe ser null
+        input.setContId(null);
         when(contenidoEducativoService.crearContenido(any(ContenidoEducativo.class)))
             .thenThrow(new jakarta.persistence.EntityNotFoundException("Usuario o curso no encontrado"));
         mockMvc.perform(post("/api/contenido-educativo")
